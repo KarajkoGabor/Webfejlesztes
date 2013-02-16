575b58ad86b117c7dd83ef464f4901ebbdffaaaa
Karajkó Gábor <karajko.gabor@gmail.com>
2013-02-16 18:13:09 +0100

    move

--- homeworks/2/js/homeWork1.js
+++ homeworks/2/js/homeWork1.js
@@ -1,5 +1,7 @@
 var gomb, outputContainer;
 var counter = 0;
+var gomb = document.getElementById('gomb');
+var w = 600, h = 400; // width and height
 
 function createNewOutput(msg) {
   var li = document.createElement('li');
@@ -8,12 +10,20 @@ function createNewOutput(msg) {
 }
 
 function onClick(ev) {
-  createNewOutput( ++counter + '. kattintás');
+  //createNewOutput( ++counter + '. kattintás');
+  onMouseMove();
+}
+
+function onMouseMove(){
+  newWidth = Math.floor(Math.random()*w);
+  newHeight = Math.floor(Math.random()*h);
+  gomb.style.position="absolute";
+  gomb.style.left=newWidth+"px";
+  gomb.style.top=newHeight+"px";
 }
 
 function setUp() {
-  gomb = document.getElementById('gomb');
-  outputContainer = document.getElementById('output');
+  //outputContainer = document.getElementById('output');
   gomb.addEventListener('mousemove', onClick);
 }
 
