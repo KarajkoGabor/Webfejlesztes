7aa2faa20e601e38392cad82c08eed09aa63df90
Karajkó Gábor <karajko.gabor@gmail.com>
2013-02-16 17:23:35 +0100

    órai

--- homeworks/2/js/homeWork1.js
+++ homeworks/2/js/homeWork1.js
@@ -1,8 +1,20 @@
-function onMouseMove()
-{
-    var s = 'X=' + window.event.clientX +  ' Y=' + window.event.clientY ;
-    document.getElementById('divCoord').innerText = s;
+var gomb, outputContainer;
+var counter = 0;
 
+function createNewOutput(msg) {
+  var li = document.createElement('li');
+  li.innerHTML = msg;
+  outputContainer.appendChild(li);
 }
 
-document.addEventListener('DOMContentLoaded', onMouseMove);
+function onClick(ev) {
+  createNewOutput( ++counter + '. kattintás');
+}
+
+function setUp() {
+  gomb = document.getElementById('gomb');
+  outputContainer = document.getElementById('output');
+  gomb.addEventListener('click', onClick);
+}
+
+document.addEventListener('DOMContentLoaded', setUp);
