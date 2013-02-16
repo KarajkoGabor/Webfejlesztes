e43698b319dbce62798434091c6ff4f581d56b5d
KarajkoGabor <karajko.gabor@gmail.com>
2013-02-16 10:37:53 -0800

    Create homeWork2.js

---
+++ homeworks/2/js/homeWork2.js
@@ -0,0 +1,42 @@
+var clickMe = {
+  gomb:null;
+
+  onClick:function(ev) { //click
+    window.alert("Ügyes vagy! De lehet hogy csaltál ;) ");
+  }
+
+  onMouseMove:function(ev){
+
+    w:800;
+    h:600;
+
+    clickMe.gomb = document.getElementById('gomb'); //gomb
+    newWidth = Math.floor(Math.random()*w); //random width
+    newHeight = Math.floor(Math.random()*h); //random height
+    clickMe.gomb.style.position="absolute"; //position
+    clickMe.gomb.style.left=newWidth+"px"; //left align
+    clickMe.gomb.style.top=newHeight+"px"; //top align
+
+    clickMe.setbackground(); // random background when hovering the button
+  }
+
+  setbackground:function ()
+  {
+    letters:'0123456789ABCDEF'.split(''); // letters
+    color:'#'; // hex first letter
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)]; //random numbers
+    }
+
+  document.getElementsByTagName("body")[0].style.backgroundColor = color; //set the color
+
+  }
+
+  setUp:function () {
+    clickMe.gomb = document.getElementById('gomb'); 
+    clickMe.gomb.addEventListener('mousemove', clickMe.onMouseMove); //hovering listener
+    clickMe.gomb.addEventListener('click', clickMe.onClick); //click listener
+  }
+
+}
+document.addEventListener('DOMContentLoaded', clickMe.setUp); //if content loaded
