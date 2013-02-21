4dd8649de6149d999796f5a9b80c946c00597996
Karajko Gabor <karajko.gabor@gmail.com>
2013-02-21 18:51:14 +0100

    órai

--- homeworks/3/js/fizzbuzzbizz.js
+++ homeworks/3/js/fizzbuzzbizz.js
@@ -1,56 +1,25 @@
-var fizzBuzzBizz = (function () {
-        "use strict";
-        var fizzOut, outputContainer,gomb;
+/*global document*/
+var myHandler = (function () {
+  "use strict";
+  var gomb, outputContainer;
+  var counter = 0;
 
-        function createNewOutput(msg) {
-            var li = document.createElement('li');
-            li.innerHTML = msg;
-            outputContainer.appendChild(li);
-        }
+  function createNewOutput(msg) {
+    var li = document.createElement('li');
+    li.innerHTML = msg;
+    outputContainer.appendChild(li);
+  }
 
-        function onClick() {
-            createNewOutput(++counter + '. kattintás');
-        }
+  function onClick() {
+    createNewOutput(++counter + '. kattintás');
+  }
 
-        /*function fBBWriter(){
+  function setUp() {
+    gomb = document.getElementById('gomb');
+    outputContainer = document.getElementById('output');
+    gomb.addEventListener('click', onClick);
+  }
+  return setUp;
+})();
 
-            for (var i = 0; i < 100; i++) {
-                    if((i % 3) == 0{
-                        Output(i + ": fizz");
-                        fizzOut = i + ": fizz";
-                    }else if((i % 7) == 0){
-                        //Output(i + ": bizz");
-                        fizzOut = i + ": bizz";
-                    }else if ((i%5) == 0){
-                        //Output(i + ": buzz");
-                        fizzOut = i + ": buzz";
-                    }
-
-                    if((i%3)==0 && (i%5) == 0)) {
-                        fizzOut = fizzOut + "buzz";
-                    }else if((i%3)==0 && (i%7) == 0)) {
-                        fizzOut = fizzOut + "bizz";
-                    }else if((i%5)==0 && (i%7) == 0)) {
-                        fizzOut = fizzOut + "bizz";
-                    }
-
-                    if((i%3)==0 && (i%5) == 0) && (i%7)==0){
-                        fizzOut = i + ": fizzbuzzbizz";
-                    }
-                    Output(fizzOut);
-                }    
-
-        } */
-
-        function setUp() {
-            gomb = document.getElementById('gomb');
-
-            outputContainer = document.getElementById('output');
-            gomb.addEventListener('click', onClick); //click listener
-        }
-
-
-        return setUp;
-
-    }());
-document.addEventListener('DOMContentLoaded', fizzBuzzBizz); //if content loaded
+document.addEventListener('DOMContentLoaded', myHandler);
