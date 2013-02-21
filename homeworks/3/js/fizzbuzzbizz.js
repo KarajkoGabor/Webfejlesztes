e0e233442d93db1b9d7f30e967af02e9982d5ca2
Karajko Gabor <karajko.gabor@gmail.com>
2013-02-21 19:09:32 +0100

    output

--- homeworks/3/js/fizzbuzzbizz.js
+++ homeworks/3/js/fizzbuzzbizz.js
@@ -16,8 +16,9 @@ var fizzBuzzBizz = (function () {
            for (i = 1; i <= 100; i++) {
                     if((i%3)== 0)
                         counter = i + ' : fizz';
+                createNewOutput(counter);
             }    
-    createNewOutput(counter);
+    
 } 
 
   function onClick() {
