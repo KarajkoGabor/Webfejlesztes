2e817856649a2eea477e5260ad190c8b9abb7a9e
Karajko Gabor <karajko.gabor@gmail.com>
2013-02-21 18:39:09 +0100

    js

---
+++ homeworks/3/js/fizzbuzzbizz.js
@@ -0,0 +1,58 @@
+var fizzBuzzBizz = (function () {
+        "use strict";
+        var fizzOut, outputContainer;
+
+        function Output(msg) {
+            var li = document.createElement('li');
+            li.innerHTML = msg;
+            outputContainer.appendChild(li);
+        }
+
+        function onClick(ev) {
+            //fBBWriter();
+            Output("out");
+
+        }
+
+        function fBBWriter(){
+
+            for (var i = 0; i < 100; i++) {
+                    if((i % 3) == 0{
+                        Output(i + ": fizz");
+                        fizzOut = i + ": fizz";
+                    }else if((i % 7) == 0){
+                        //Output(i + ": bizz");
+                        fizzOut = i + ": bizz";
+                    }else if ((i%5) == 0){
+                        //Output(i + ": buzz");
+                        fizzOut = i + ": buzz";
+                    }
+
+                    if((i%3)==0 && (i%5) == 0)) {
+                        fizzOut = fizzOut + "buzz";
+                    }else if((i%3)==0 && (i%7) == 0)) {
+                        fizzOut = fizzOut + "bizz";
+                    }else if((i%5)==0 && (i%7) == 0)) {
+                        fizzOut = fizzOut + "bizz";
+                    }
+
+                    if((i%3)==0 && (i%5) == 0) && (i%7)==0){
+                        fizzOut = i + ": fizzbuzzbizz";
+                    }
+                    Output(fizzOut);
+                }    
+
+        } 
+
+        function setUp() {
+            gomb = document.getElementById('gomb');
+
+            outputContainer = document.getElementById('fbb_output');
+            gomb.addEventListener('click', onClick); //click listener
+        }
+
+
+        return setUp;
+
+    }());
+document.addEventListener('DOMContentLoaded', fizzBuzzBizz); //if content loaded
