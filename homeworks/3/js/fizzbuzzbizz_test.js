3fff0b5f51ee5f01949a8e356523bb87fefd5949
Karajkó Gábor <karajko.gabor@gmail.com>
2013-02-22 15:19:35 +0100

    test first run

---
+++ homeworks/3/js/fizzbuzzbizz_test.js
@@ -0,0 +1,23 @@
+
+(function() {
+
+  $().ready(function() {
+    module("FizzBuzzBizz generátor");
+    test("3==fizz", function() {
+      return equal(fizzBuzzBizz.fbb(3), 'fizz');
+    });
+    /*test("1==1", function() {
+      return equal(fibonacci(1), 1);
+    });
+    test("2==1", function() {
+      return equal(fibonacci(2), 1);
+    });
+    test("5==5", function() {
+      return equal(fibonacci(5), 5);
+    });*/
+    return test("13==13", function() {
+      return equal(fizzBuzzBizz.fbb(13), 13);
+    });
+  });
+
+}).call(this);
