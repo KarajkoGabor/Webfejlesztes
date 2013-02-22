2a295f83a90e38a018f54c80da9bac8825435b2c
Karajkó Gábor <karajko.gabor@gmail.com>
2013-02-22 16:27:20 +0100

    more test

--- homeworks/3/js/fizzbuzzbizz_test.js
+++ homeworks/3/js/fizzbuzzbizz_test.js
@@ -6,15 +6,21 @@
     test("3==fizz", function() {
       return equal(fbb(3), 3 +' : fizz');
     });
-    /*test("1==1", function() {
-      return equal(fibonacci(1), 1);
+    test("15==fizzbuzz", function() {
+      return equal(fbb(15), 15 +' : fizzbuzz');
     });
-    test("2==1", function() {
-      return equal(fibonacci(2), 1);
+    test("21==fizzbizz", function() {
+      return equal(fbb(21), 21 +' : fizzbizz');
     });
-    test("5==5", function() {
-      return equal(fibonacci(5), 5);
-    });*/
+    test("80==buzz", function() {
+      return equal(fbb(80), 80 +' : buzz');
+    });
+    test("91==bizz", function() {
+      return equal(fbb(91), 91 +' : bizz');
+    });
+    test("35==buzzbizz", function() {
+      return equal(fbb(35), 35 +' : buzzbizz');
+    });
     return test("13==13", function() {
       return equal(fbb(13), 13);
     });
