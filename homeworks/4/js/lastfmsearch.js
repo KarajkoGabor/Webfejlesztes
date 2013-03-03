badbc34601dd48ee8ca1f3b5176837115fd0d085
Karajko Gabor <karajko.gabor@gmail.com>
2013-03-03 22:38:38 +0100

    try a köbön

--- homeworks/4/js/lastfmsearch.js
+++ homeworks/4/js/lastfmsearch.js
@@ -1,26 +1,57 @@
-(function() {
+/*(function() {
 
-          var artist;
+          var lastFm
 
-          artist = (function(){
+          lastFm = (function(){
 
-            function artist(){}
+            function lastFm(){}
 
-            artist.key="";
+            //artist.key="";
 
-            artist.getInfo = function(name, callback) {
+            lastFm.getInfo = function(name, callback) {
             var url;
-            url = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Metallica&api_key=dd5a33741258754e14204cbe9c9bdf1d&format=json";
+            url = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Metallica&api_key=dd5a33741258754e14204cbe9c9bdf1d&format=json';
             return $.ajax({
-              dataType: "json",
+              type : 'GET',
+              dataType: 'json',
               url: url,
-              data: null,
               success: callback,
               crossDomain: true
             });
             };
 
-             return artist;
+             return lastFm;
       })();
 
-        }).call(this);
+        window.lastFm = lastFm;
+
+        }).call(this);*/
+    (function(){
+            "use strict";
+            var lastFm;
+            lastFm  = (function(){
+                    function lastFm(){}
+     
+                    LastFm.search = function(query, callback){
+                            return $.ajax({
+                                    type: 'GET',
+                                    datatype: 'json',
+                                    url: 'http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=' + query + '&api_key=dd5a33741258754e14204cbe9c9bdf1d&format=json',
+                                    success: callback,
+                                    crossDomain: true
+                            });
+                    };
+     
+                    lastFm.getInfo = function(artist, callback){
+                            return $.ajax({
+                                    type: 'GET',
+                                    dataType: 'json',
+                                    url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist + '&api_key=dd5a33741258754e14204cbe9c9bdf1d&format=json',                                    crossDomain: true
+                            });
+                    };
+     
+                    return lastFm;
+            })();
+     
+            window.lastFm = lastFm;
+    }).call(this);
