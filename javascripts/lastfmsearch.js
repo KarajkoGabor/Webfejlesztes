d91aa3d7e6822ff29dba19685cea74ce1427d93d
Karajko Gabor <karajko.gabor@gmail.com>
2013-03-03 22:36:40 +0100

    try try try

--- javascripts/lastfmsearch.js
+++ javascripts/lastfmsearch.js
@@ -32,7 +32,7 @@
             lastFm  = (function(){
                     function lastFm(){}
      
-                    /*LastFm.search = function(query, callback){
+                    LastFm.search = function(query, callback){
                             return $.ajax({
                                     type: 'GET',
                                     datatype: 'json',
@@ -40,13 +40,13 @@
                                     success: callback,
                                     crossDomain: true
                             });
-                    };*/
+                    };
      
                     lastFm.getInfo = function(artist, callback){
                             return $.ajax({
                                     type: 'GET',
                                     dataType: 'json',
-                                    url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist + '&api_key=89e0b564ee2e75b41989a182288d7704&format=json',                                 crossDomain: true
+                                    url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist + '&api_key=dd5a33741258754e14204cbe9c9bdf1d&format=json',                                    crossDomain: true
                             });
                     };
      
