15f191d515eeafb0ae550dee0ed0db038335d158
Karajko Gabor <karajko.gabor@gmail.com>
2013-03-17 20:16:55 +0100

    grunt project

---
+++ homeworks/5/README.md
@@ -0,0 +1,30 @@
+# gruntBuildTitl
+
+The best jQuery plugin ever.
+
+## Getting Started
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/5/grunt/master/dist/gruntBuild.min.js
+[max]: https://raw.github.com/5/grunt/master/dist/gruntBuild.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/gruntBuild.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ homeworks/5/src/fizzbuzzbizz.js
@@ -0,0 +1,56 @@
+/*global document*/
+var fizzBuzzBizz = (function () {
+  "use strict";
+  var outputContainer;
+  var counter = 0;
+  var i = 0;
+  
+
+  function createNewOutput(msg) {
+    var li = document.createElement('li');
+    li.innerHTML = msg;
+    outputContainer.appendChild(li);
+  }
+
+  function fbb(number){
+    counter = 0;
+    if((number%3)== 0)
+        counter = number + ' : fizz';
+    if((number%5)== 0)
+        counter = number + ' : buzz';
+    if((number%7)== 0)
+         counter = number + ' : bizz';
+    if((number%3)== 0 && (number%5)==0)
+         counter = number + ' : fizzbuzz';
+    if((number%3)== 0 && (number%7)==0)
+         counter = number + ' : fizzbizz';
+    if((number%7)== 0 && (number%5)==0)
+        counter = number + ' : buzzbizz';
+    if((number%3)== 0 && (number%5)==0 && (number%7)==0)
+         counter = number + ' : fizzbuzzbizz';
+    if(counter == 0)
+         counter = number;
+
+    return counter;
+
+  }
+
+  function fBBWriter(){
+
+           for (i = 1; i <= 100; i++) {
+                fbb(i);   
+                createNewOutput(counter);
+            }    
+    
+  } 
+
+  function setUp() {
+    //gomb = document.getElementById('gomb');
+    outputContainer = document.getElementById('output');
+    //gomb.addEventListener('click', onClick);
+    fBBWriter();
+  }
+  return setUp;
+})();
+
+document.addEventListener('DOMContentLoaded', fizzBuzzBizz);
